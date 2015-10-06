ab96b7f97ff8052bb56bdf6bc417c3ac26d678c0
cwr.prabhat.tiwari <cwr.prabhat.tiwari@asurion.com>
2015-10-06 17:16:03 -0500

    Added RestController and exposed as RestService

--- src/main/java/com/controller/CountryController.java
+++ src/main/java/com/controller/CountryController.java
@@ -4,18 +4,16 @@ import java.util.List;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.ui.ModelMap;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.model.Country;
 import com.service.CountryService;
 
-@org.springframework.stereotype.Controller
+@RestController
 public class CountryController {
 
 	final static Logger logger = LoggerFactory.getLogger(CountryController.class);
@@ -23,26 +21,12 @@ public class CountryController {
 	@Autowired
 	private CountryService CountryService;
 	
-  /* @RequestMapping(value="/")
-	    public ModelAndView Register() {
-	        List<Country> listUsers = CountryService.getAllCountries();
-	        ModelAndView model = new ModelAndView("Register");
-	        model.addObject("userList", listUsers);
-	        return model;
-	    }*/
+
 	
-	 @RequestMapping(value = { "/", "/list" }, method = RequestMethod.GET)
-	    public String listUsers(ModelMap model) {
-		 logger.info("Entered in listUsers");
-	     List<Country> listUsers = CountryService.getAllCountries();
-	        model.addAttribute("userList", listUsers);
-	        logger.info("exit in listUsers");
-	        return "Register";
-	    } 
-	 
-   @RequestMapping(value="/greeting",produces="application/json")
-   public  ResponseEntity<List<Country>> greeting()  {
-	   List<Country> listUsers = CountryService.getAllCountries();
-       return new ResponseEntity<List<Country>>(listUsers, HttpStatus.OK);
-   }
+	 @RequestMapping(value="/countryList", method = RequestMethod.GET, produces = "application/json")
+	   public  @ResponseBody List<Country> greeting()  {
+		   List<Country> listUsers = CountryService.getAllCountries();
+	       return listUsers;
+	   }
+ 
 }
